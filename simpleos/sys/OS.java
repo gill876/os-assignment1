780a8aa0f89589c7c94f4f0141c83784f2ffe5a1
Cargill Seiveright <microcargill@hotmail.com>
2020-10-11 04:47:20 -0500

    Make Fibonacci sequence

--- simpleos/sys/OS.java
+++ simpleos/sys/OS.java
@@ -9,9 +9,41 @@ public class OS {
     public static void main(String[] args){
 
         try {
-            int instructions[] = {1001, 800, 2004, 5001, 0, 100, 4005, 3000, 3000, 7000, 6009, 6005,9000};
-            int stdin[] = {4, 5};
-            MyMemory m = new MyMemory(5);
+            // 1 = a
+            // 2 = b
+            int instructions[] = {
+                // Load AC from stdin
+                3000,
+                // Store AC to memory
+                2001,
+                // Load AC from stdin
+                3000,
+                // Store AC to memory
+                2002,
+                /* REPEAT */
+                // Load AC from memory
+                1001,
+                // Store AC to stdout
+                7000,
+                // Add to AC from memory
+                5002,
+                // Store AC to memory
+                2001,
+                // Load AC from memory
+                1001,
+                // Subtract from AC from memory
+                4002,
+                // Store AC to memory
+                2002,
+                // GOTO
+                6004,
+                // GOTO
+                6010,
+                // Halt
+                9000
+            };
+            int stdin[] = {0, 1};
+            MyMemory m = new MyMemory(instructions.length);
             m.loadValues(instructions);
             MyProcessor p = new MyProcessor(m, stdin);
             m.printSize();
